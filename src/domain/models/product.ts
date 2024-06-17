5f170d4070e8edb5f27431524a4672bf94f9314d
Lucas Miranda <lucasmirandaneves@gmail.com>
2024-06-17 17:45:49 -0300

    feat: implements Product model

---
+++ src/domain/models/product.ts
@@ -0,0 +1,49 @@
+import { ValidationError } from "../errors/validation-error";
+import { Sale } from "./sale";
+
+interface Input {
+  id?: string;
+  restaurantId: string;
+  name: string;
+  price: number;
+  category: string;
+  sale?: {
+    id?: string;
+    productId: string;
+    price: number;
+    description: string;
+    hours: {
+      start: string;
+      end: string;
+      days: number[];
+    }[];
+  };
+}
+
+export class Product {
+  constructor(
+    readonly id: string,
+    readonly restaurantId: string,
+    readonly name: string,
+    readonly price: number,
+    readonly category: string,
+    readonly sale: Sale | null
+  ) {
+    if (!this.id) throw new ValidationError('Product id is required');
+    if (!this.restaurantId) throw new ValidationError('Product restaurantId is required');
+    if (!this.name) throw new ValidationError('Product name is required');
+    if (!this.price) throw new ValidationError('Product price is required');
+    if (!this.category) throw new ValidationError('Product category is required');
+  }
+
+  static create({ id, restaurantId, name, price, category, sale }: Input) {
+    return new Product(
+      id || crypto.randomUUID(),
+      restaurantId,
+      name,
+      price,
+      category,
+      sale ? Sale.create(sale) : null
+    );
+  }
+}
