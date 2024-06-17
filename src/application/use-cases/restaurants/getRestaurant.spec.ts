4ed61f8f5d08c0b64965863a314ca32d6ecec7c3
Lucas Miranda <lucasmirandaneves@gmail.com>
2024-06-17 17:58:00 -0300

    feat: implements Get Restaurant by ID use case

---
+++ src/application/use-cases/restaurants/getRestaurant.spec.ts
@@ -0,0 +1,41 @@
+import { describe } from "node:test";
+import { beforeAll, expect, it } from "vitest";
+import { setupDatabase } from "../../../infraestructure/database/setup";
+import { DatabaseRestaurantRepository } from "../../../domain/repositories/restaurantRepository/databaseRestaurantRepository";
+import Restaurant from "../../../domain/models/restaurant";
+import GetRestaurant from "./getRestaurant";
+import Hour from "../../../domain/models/hour";
+
+describe('Get Restaurant Use Case', () => {
+  const databaseRestaurantRepository = new DatabaseRestaurantRepository();
+  const getRestaurant = new GetRestaurant(databaseRestaurantRepository);
+  const restaurantData = Restaurant.create({
+    id: '1',
+    name: 'Restaurant 1',
+    address: 'Address 1',
+    hours: [
+      Hour.create({
+        start: '08:00',
+        end: '18:00',
+        days: [1, 2, 3, 4, 5],
+      }),
+      Hour.create({
+        start: '08:00',
+        end: '12:00',
+        days: [6, 0],
+      }),
+    ]
+  });
+
+  beforeAll(async () => {
+    await setupDatabase();
+    
+    await databaseRestaurantRepository.create(restaurantData);
+  });
+
+  it('should return a Restaurant by id', async () => {
+    const restaurant = await getRestaurant.execute(restaurantData.id);
+
+    expect(restaurant).toStrictEqual(restaurantData);
+  });
+});
