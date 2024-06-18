3edb38f524832f0d5443002bd69427cfc441f108
Lucas Miranda <lucasmirandaneves@gmail.com>
2024-06-18 11:59:10 -0300

    feat: implements Image Controller

--- src/infraestructure/web/server.ts
+++ src/infraestructure/web/server.ts
@@ -1,14 +1,16 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
+import imageRouter from '../../interfaces/controllers/image';
 import { setupDatabase } from '../database/setup';
+import bodyParser from 'body-parser';
+import express from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-console.log('iniciando server');
-
 const app = express();
+
 app.use(bodyParser.json());
+
+app.use('/image', imageRouter);
 
 app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
   const status = err.status || 500;
