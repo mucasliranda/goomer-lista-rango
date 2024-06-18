d5179436260114fc94801694bf7e1d9b537d3d31
Lucas Miranda <lucasmirandaneves@gmail.com>
2024-06-18 11:48:39 -0300

    feat: implements get Image on local repository

--- src/domain/repositories/imageRepository/index.ts
+++ src/domain/repositories/imageRepository/index.ts
@@ -1,6 +1,7 @@
 import { ImageFile } from "../../models/imageFile";
 
 export abstract class ImageRepository {
+  abstract get(id: string): Promise<any>;
   abstract save(image: ImageFile): Promise<void>;
   abstract delete(id: string): Promise<void>;
 }
--- src/domain/repositories/imageRepository/localImageRepository.ts
+++ src/domain/repositories/imageRepository/localImageRepository.ts
@@ -6,6 +6,11 @@ import fs from 'fs';
 export class LocalImageRepository implements ImageRepository {
   constructor() {}
 
+  async get(id: string): Promise<string> {
+    const filePath = path.join('public/images-database', id + '.jpg');
+    return fs.readFileSync(filePath).toString('base64');
+  }
+
   async save(image: ImageFile): Promise<void> {
     const filePath = path.join('public/images-database', image.id + '.jpg');
     fs.writeFileSync(filePath, image.buffer);
